eccaeafdba018b2220f314776a4ae4535f01960b
Charles Emrich <libertinistic@gmail.com>
2016-05-16 18:38:16 -0700

    Finished data generation logic

--- Week2/Pizza/data-model.js
+++ Week2/Pizza/data-model.js
@@ -1,20 +1,4 @@
 //Global Variables
-// var pizzasMade = [
-//   getRandomInt(0,4),
-//   getRandomInt(0,7),
-//   getRandomInt(2,15),
-//   getRandomInt(15,35),
-//   getRandomInt(12,31),
-//   getRandomInt(5,20),
-// ];
-// var deliveriesMade = [
-//   getRandomInt(0,pizzasMade[0]), //I'm not sure about this.
-//   getRandomInt(0.4),
-//   getRandomInt(1,4),
-//   getRandomInt(3,8),
-//   getRandomInt(5,12),
-//   getRandomInt(5,11),
-// ];
 var deliveryMax = [4,4,4,8,12,11];
 var deliveryMin = [0,0,1,3,5,6];
 
@@ -23,6 +7,7 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+//Store Constructor
 function Store (loc) {
   this.location = loc;
   this.pizzas = [
