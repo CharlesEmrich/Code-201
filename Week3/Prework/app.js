df7d3a238c65e56d0cfe9b988ed483f9338175a5
Charles Emrich <libertinistic@gmail.com>
2016-05-22 17:24:36 -0700

    attempted to add counter, broke some stuff

--- Week3/Prework/app.js
+++ Week3/Prework/app.js
@@ -1,20 +1,44 @@
 // Global Variables
 var imgOne = document.getElementById('imgOne');
 var imgTwo = document.getElementById('imgTwo');
+var imgOneCount = document.getElementById('imgOneCount');
+var imgTwoCount = document.getElementById('imgTwoCount');
+var images = [
+    ['img/opt1.jpeg', 0],
+    ['img/opt2.jpg', 0],
+    ['img/opt3.jpg', 0],
+    ['img/opt4.png', 0],
+    ['img/opt5.jpg', 0]
+];
 
 // Functions
 function getRand() {
-  var a =  Math.floor(5 * Math.random() + 1);
-  console.log (a);
+  var a =  Math.floor(5 * Math.random());
+  // console.log (a);
   return a;
 }
 
-function imgRefresh(a) {
-  a.setAttribute('src','img/opt' + getRand() + '.jpg');
+function imgRefresh(a,b) {
+  a.setAttribute('src', images[b][0]);
 }
 
 //User Interaction
+imgOne.addEventListener('click', function () {
+  images[r][1] += 1; //Does this work?
+  var r = getRand();
+  imgRefresh(imgOne, r);
+  imgOneCount.textContent = images[r][1];
+}, false);
+imgTwo.addEventListener('click', function () {
+  images[r][1] += 1;
+  var r = getRand();
+  imgRefresh(imgTwo, r);
+}, false);
+// imgOne.onclick = imgRefresh;
+// imgTwo.onclick = imgRefresh;
+
+//Something here to coerce things such that no two things can have the same image?
 
 //Beginning State
-imgRefresh(imgOne);
-imgRefresh(imgTwo);
+imgRefresh(imgOne, getRand());
+imgRefresh(imgTwo, getRand());
