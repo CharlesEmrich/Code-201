959c843f5bcae0e210c7d7838e109fafd23f9911
Charles Emrich <libertinistic@gmail.com>
2016-05-23 22:07:01 -0700

    html scaffold, beginnings of JS and CSS

---
+++ Week3/busMall/app.js
@@ -0,0 +1,15 @@
+// Cached DOM queries
+var imgOne = document.getElementById('imgOne');
+var imgTwo = document.getElementById('imgTwo');
+var imgThree = document.getElementById('imgThree');
+var imgOneCount = document.getElementById('imgOneCount');
+var imgTwoCount = document.getElementById('imgTwoCount');
+var imgThreeCount = document.getElementById('imgThreeCount');
+var buttonBar = gebi('buttonBar');
+
+var plot = gebi('plot');
+
+//Frequently Used Functions
+var gebi = function (el) {
+  return document.getElementById(el);
+};
