48478fabdfe71cbe1dfca8fb625d8e9d169145e1
Charles Emrich <libertinistic@gmail.com>
2016-05-23 19:51:11 -0700

    commiting changes from class today

--- Week3/Prework/app.js
+++ Week3/Prework/app.js
@@ -17,6 +17,7 @@ function getRand() {
   // console.log (a);
   return a;
 }
+//add different image forcing to this function
 
 function imgRefresh(a,b) {
   a.setAttribute('src', images[b][0]);
@@ -25,21 +26,25 @@ function imgRefresh(a,b) {
 //Beginning State
 var r = getRand();
 var rr = getRand();
+// if r === rr
 imgRefresh(imgOne, r);
 imgRefresh(imgTwo, rr);
 
 //User Interaction
 imgOne.addEventListener('click', function () {
   images[r][1] ++;
+  // if length thingsSHown = length possible outcomes, reset thingsshown
   r = getRand();
+  //if it was already there, reroll R
+  //push r to thingsWeveShown
   imgRefresh(imgOne, r);
   imgOneCount.textContent = images[r][1];
 }, false);
 imgTwo.addEventListener('click', function () {
   images[rr][1] ++;
   rr = getRand();
-  imgRefresh(imgOne, rr);
-  imgOneCount.textContent = images[rr][1];
+  imgRefresh(imgTwo, rr);
+  imgTwoCount.textContent = images[rr][1];
 }, false);// imgOne.onclick = imgRefresh;
 // imgTwo.onclick = imgRefresh;
 
